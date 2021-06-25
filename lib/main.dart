5edb69251814da319a184529b6b0d81feec47b4c
Leonardo <leonardosartori22@hotmail.com>
2021-06-24 22:48:47 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false, //desabilita a faixa do debug
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var _frases = [
+    "A gratidão é a memória do coração.",
+    "Não importa o que você decidiu. O que importa é que isso te faça feliz.",
+    "Deixe pra trás o que não te leva pra frente."
+  ];
+
+  var _fraseGerada = "Clique abaixo para gerar uma frase";
+
+  void _gerarFrase() {
+    var numeroSorteado = Random().nextInt(_frases.length);
+    setState(() {
+      _fraseGerada = _frases[numeroSorteado];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //esqueleto
+      appBar: AppBar(
+        title: Text("Frases do dia"),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        width: double.infinity, //100%
+        decoration: BoxDecoration(
+            border:
+                Border.all(width: 3, color: Colors.green) //borda do container
+            ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("images/logo.png"),
+            Text(
+              _fraseGerada,
+              textAlign: TextAlign.justify,
+              style: TextStyle(
+                  fontSize: 17,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black),
+            ),
+            RaisedButton(
+              child: Text(
+                "Nova Frase",
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              color: Colors.green,
+              onPressed: _gerarFrase,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
